3174d85a8b7299f615dbb7ea12eb5611a22bb8dd
Mohamed Shalan <mshalan@aucegypt.edu>
2023-10-06 15:44:09 +0300

    Minor edits

--- verify/utb/EF_PWM32_tb.v
+++ verify/utb/EF_PWM32_tb.v
@@ -1,4 +1,26 @@
 
+/*
+        Copyright 2023 efabless
+
+        Author: Mohamed Shalan (mshalan@aucegypt.edu)
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+
+*/
+
+`timescale			    1ns/1ns
+`default_nettype        none
+
 module EF_PWM32_tb;
     reg  clk;
     reg  rst_n;
@@ -28,13 +50,14 @@ module EF_PWM32_tb;
     reg [1:0] pwmB_e5a;
 
     initial clk = 0;
-    always #5 clk <= !clk;
+    always #10 clk <= !clk;
 
     initial begin
         $dumpfile("pwm.vcd");
         $dumpvars;
     end
 
+    
     initial begin
         rst_n = 0;
         clkdiv = 4'b0010;
