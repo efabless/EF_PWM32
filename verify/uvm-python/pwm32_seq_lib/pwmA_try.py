c2f318d2c8e0241fbbdf4c510534dc1fe4758c52
M0stafaRady <mostafa.rady@efabless.com>
2024-10-08 18:22:09 +0300

    change the sequence randmization

--- verify/uvm-python/pwm32_seq_lib/pwmA_try.py
+++ verify/uvm-python/pwm32_seq_lib/pwmA_try.py
@@ -17,16 +17,21 @@ class pwmA_try(bus_seq_base):
         # get register names/address conversion dict
         await super().body()
         three_rand = sorted(random.sample(range(1, 0xFF), 3))
+        print(f"three_rand = {three_rand}")
+        three_rand = [26, 84, 118]
+        GENA = 0b100100
+        GENB = 0b011011
         # enable control
         await self.send_req(is_write=True, reg="CLKGATE", data_condition=lambda data: data == 1)
         
         await self.send_req(is_write=True, reg="CLKDIV", data_condition=lambda data: data in [0b1, 0b10, 0b100, 0b1000])
+        # await self.send_req(is_write=True, reg="CLKDIV", data_condition=lambda data: data in [0b100])
         await self.send_req(is_write=True, reg="TOP", data_condition=lambda data: data == three_rand[2])
-        await self.send_req(is_write=True, reg="GENA")
-        await self.send_req(is_write=True, reg="GENB")
+        await self.send_req(is_write=True, reg="GENA", data_condition=lambda data: data == GENA)
+        await self.send_req(is_write=True, reg="GENB", data_condition=lambda data: data == GENB)
         await self.send_req(is_write=True, reg="CMPA", data_condition=lambda data: data == three_rand[0])
         await self.send_req(is_write=True, reg="CMPB", data_condition=lambda data: data == three_rand[1])
-        await self.send_req(is_write=True, reg="CONTROL", data_condition=lambda data: data & 0b111 == 0b111)
+        await self.send_req(is_write=True, reg="CONTROL", data_condition=lambda data: data == 0b111)
         await Timer(11500, "ns")
         # await self.send_req(is_write=True, reg="GENA")
         await Timer(515500, "ns")
